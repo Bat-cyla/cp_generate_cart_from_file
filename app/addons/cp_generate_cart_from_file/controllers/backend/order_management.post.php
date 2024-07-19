9df6b17b888c68616d2f7b9675fe5b0edd7331a2
Nemov <cp.nemov@gmail.com>
2024-07-19 18:17:37 +0400

    Реализован импорт на странице редактирования и добавления заказа в панели администратора

--- app/addons/cp_generate_cart_from_file/controllers/backend/order_management.post.php
+++ app/addons/cp_generate_cart_from_file/controllers/backend/order_management.post.php
@@ -1,39 +1,17 @@
 <?php
-/*****************************************************************************
- *                                                        © 2013 Cart-Power   *
- *           __   ______           __        ____                             *
- *          / /  / ____/___ ______/ /_      / __ \____ _      _____  _____    *
- *      __ / /  / /   / __ `/ ___/ __/_____/ /_/ / __ \ | /| / / _ \/ ___/    *
- *     / // /  / /___/ /_/ / /  / /_/_____/ ____/ /_/ / |/ |/ /  __/ /        *
- *    /_//_/   \____/\__,_/_/   \__/     /_/    \____/|__/|__/\___/_/         *
- *                                                                            *
- *                                                                            *
- * -------------------------------------------------------------------------- *
- * This is commercial software, only users who have purchased a valid license *
- * and  accept to the terms of the License Agreement can install and use this *
- * program.                                                                   *
- * -------------------------------------------------------------------------- *
- * website: https://store.cart-power.com                                      *
- * email:   sales@cart-power.com                                              *
- ******************************************************************************/
-
 use Tygh\CpFileReader\CpXlsColumnException;
 use Tygh\Tygh;
 use Tygh\Storage;
 use Tygh\CpFileReader\CpFileReader;
 use Tygh\CpFileUploader\CpFileUploader;
 
-if (!defined('BOOTSTRAP')) {
-    die('Access denied');
-}
-
 $view = Tygh::$app['view'];
 $cart = &Tygh::$app['session']['cart'];
 $auth = &Tygh::$app['session']['auth'];
 
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($mode == 'generate') {
-
+    if($mode == 'generate') {
         if (isset($_FILES, $_FILES['cp_generate_attach'])) {
             $params = $_REQUEST;
             $file = $_FILES['cp_generate_attach'];
@@ -76,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
 
             if (!empty($cart_data)) {
+
                 fn_add_product_to_cart($cart_data, $cart, $auth, true);
                 fn_calculate_cart_content($cart, $auth, 'E', true, 'F', true);
                 fn_save_cart_content($cart, $auth['user_id']);
@@ -89,18 +68,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 ]);
                 $view->display('addons/cp_generate_cart_from_file/views/variations.tpl');
 
-                exit();
+
             } else {
                 Tygh::$app['ajax']->assign('non_ajax_notifications', true);
                 fn_set_notification('N', __('notice'), __("cp_import_success"));
-                Tygh::$app['ajax']->assign('force_redirection', fn_url('checkout.cart'));
-
+                Tygh::$app['ajax']->assign('force_redirection', fn_url('order_management.update'));
                 exit();
             }
         }
     }
-
-    if ($mode == 'finish') {
+    elseif ($mode == 'finish') {
         $params = $_REQUEST;
         if (!empty($params['chosen_products'])) {
             $chosen_products = $params['chosen_products'];
@@ -116,24 +93,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         Tygh::$app['ajax']->assign('non_ajax_notifications', true);
         fn_set_notification('N', __('notice'), __("cp_import_success"));
-        Tygh::$app['ajax']->assign('force_redirection', fn_url('checkout.cart'));
+        Tygh::$app['ajax']->assign('force_redirection', fn_url('order_management.update'));
 
         exit();
     }
 }
-
 if ($mode == 'view') {
-    if (defined('AJAX_REQUEST')) {
-        if (empty($auth['user_id'])) {
-            Tygh::$app['ajax']->assign('force_redirection', fn_url('auth.login_form'));
-        } else {
-            $template_data = fn_cp_generate_cart_from_file_get_default_template_data();
-            $view->assign([
-                'template_data' => $template_data,
-            ]);
-            $view->display('addons/cp_generate_cart_from_file/views/view.tpl');
-        }
-    }
 
-    exit();
+    $template_data = fn_cp_generate_cart_from_file_get_default_template_data();
+    $view->assign([
+        'template_data' => $template_data,
+    ]);
+    $view->display('addons/cp_generate_cart_from_file/views/view.tpl');
+}elseif($mode == 'export'){
+    fn_print_die(1111);
 }
---
+++ design/backend/templates/addons/cp_generate_cart_from_file/hooks/order_management/update_tools_list.pre.tpl
@@ -0,0 +1,19 @@
+<li>
+    {btn type="dialog"
+    class="import_cart_modal"
+    text=__("cp_load_products_from_file")
+    href="{"order_management.view"|fn_url}"
+    target_id="cp_generate_cart_modal"
+    }
+</li>
+<li>
+    {btn type="list"
+    text=__("cp_export_pdf")
+    href="{"order_management.export"|fn_url}"
+    }
+</li>
+
+
+
+
+
